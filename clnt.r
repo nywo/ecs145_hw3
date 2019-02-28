50ec544de6f7c3829d8be7bf281694c8d1b1a3e6
Nicholas Wong <nicholaswong@campus-047-235.ucdavis.edu>
2019-02-27 17:52:48 -0800

    ol function working

---
+++ clnt.r
@@ -0,0 +1,33 @@
+otpClient <- function(host="localhost",port=2002){
+	#open connection
+	con <- socketConnection(host=host, port = port, blocking=TRUE,server=FALSE, open="r+")
+
+	while(TRUE){
+		f = readline(prompt="#otp ") #accept user input
+
+		if(f == "ol") {
+			serialize('ol',con)
+			result <- unserialize(con)
+			print(result)
+		} else if (f == "get") {
+			serialize('get', con)
+			serialize(request,con)
+			result<-unserialize(con)
+			print(result)
+		} else if (f == "put") {
+			serialize('put', con)
+			serialize(object, con)
+			print("Put Finished\n")
+			print(result)
+		} else if (f == "exit") {
+			close(con)
+			print("Connection Closed\n")
+		} else {
+			print("Error: request not recognized")
+
+		}
+	}
+
+}
+
+otpClient()
---
+++ srvr.r
@@ -0,0 +1,48 @@
+OptServer <- function(nclnt, port=2002){
+	#initialize data frame with names, code courtesy of
+	#https://stackoverflow.com/questions/32712301/create-empty-data-frame-with-column-names-by-assigning-a-string-vector
+	master_data <- data.frame(matrix(ncol = 3, nrow = 0))
+	names <- c("ObjectName", "NBytes", "ObjectClass")
+	colnames(master_data) <- names
+
+	while(TRUE){
+		con <- socketConnection(host="localhost", port = port, blocking=TRUE,server=TRUE, open="r+")
+		order <- unserialize(con)
+
+		if(order == "ol"){
+			print("Ol called")
+			serialize(master_data,con)
+		}
+
+		else if(order == "put"){
+			print("put called")
+			object <- unserialize(con)
+			put(object)
+		}
+
+		else if(order == "get"){
+			print("get called")
+			request<-unserialize(con)
+			get(request)
+		}
+	}
+#separate data into function type (ol, get, or put) and do
+#certain things based on the type
+
+	put <- function(stuff) {
+		nbyte <- object.size(stuff) #returns size of object
+		classtype <- class(stuff) #returns type of object
+		stuff2put <- c(stuff, nbyte, classtype) #makes a list of object's properties
+		master_data 
+	}
+
+	get <- function(request){
+		serialize(,con)
+	}
+
+# response <- toupper(data) 
+#     writeLines(response, con) 
+#     close(con)
+
+}
+OptServer()
